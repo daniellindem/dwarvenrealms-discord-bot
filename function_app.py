d4ae4847a7cf8f0e229d19bd26017d7619bc7721
Daniel <daniel.lindemann98@gmail.com>
2024-04-09 22:18:43 +0200

    First test for leaderboard function

--- function_app.py
+++ function_app.py
@@ -164,8 +164,127 @@ def rupturecalc(rupturelevel, rerollcost):
     except Exception as e:
         logging.error(f"Error occurred in rupturecalc: {e}")
         return f"Error occurred in rupturecalc: {e}"
+    
+def get_user_characters(username: str):
+    # URLs for normal and hardcore leaderboards
+    softcore_url = "http://loadbalancer-e2a9b2a-1115437761.us-east-1.elb.amazonaws.com/leaderboards/scores?type=normal"
+    hardcore_url = "http://loadbalancer-e2a9b2a-1115437761.us-east-1.elb.amazonaws.com/leaderboards/scores?type=hardcore"
+
+    # Make requests to both URLs
+    softcore_response = requests.get(softcore_url)
+    hardcore_response = requests.get(hardcore_url)
+
+    username_lower = username.lower()
+
+    # Check if requests were successful
+    if softcore_response.status_code == 200 and hardcore_response.status_code == 200:
+        # Parse JSON responses
+        softcore_data = softcore_response.json()
+        hardcore_data = hardcore_response.json()
+
+        user_info_list = []
+        sc_ranking = 1
+        hc_ranking = 1
+
+        # Search for username in normal leaderboard
+        for leaderboard in softcore_data["leaderboards"]:
+            if str(leaderboard["name"].split()[0]).lower() == username_lower:
+                user_info_list.append({"leaderboard_type": "Softcore", "character_info": leaderboard, "ranking": sc_ranking})
+            sc_ranking += 1
+
+        # Search for username in hardcore leaderboard
+        for leaderboard in hardcore_data["leaderboards"]:
+            if str(leaderboard["name"].split()[0]).lower() == username_lower:
+                user_info_list.append({"leaderboard_type": "Hardcore", "character_info": leaderboard, "ranking": hc_ranking})
+            hc_ranking += 1
+
+        if user_info_list:
+            return user_info_list
+        else:
+            return f"No characters found for user '{username}' in leaderboards."
+    else:
+        return "Failed to retrieve leaderboard data."
+    
+
+    
+def format_character_info_base(highest_characters):
+    highest_characters_sorted = sorted(highest_characters, key=lambda x: int(x["character_info"]["raptureLevel"]), reverse=True)
+    #print(highest_characters_sorted)
+    message = ""
+    for character in highest_characters_sorted:
+        if character["leaderboard_type"] == "Hardcore":
+            if character["character_info"]["deaths"] == "0":
+                alive_status = "Alive"
+            else:
+                alive_status = "Dead"
+            message += f"""**Name:** {character["character_info"]["name"]}
+**Rapture Level:** {character["character_info"]["raptureLevel"]}
+**Ranking:** {character["ranking"]}
+**Leaderboard:** {character["leaderboard_type"]}
+**Status:** {alive_status}
+**ID:** {character["character_info"]["id"]}
+\n"""
+        else:
+            message += f"""**Name:** {character["character_info"]["name"]}
+**Rapture Level:** {character["character_info"]["raptureLevel"]}
+**Ranking:** {character["ranking"]}
+**Leaderboard:** {character["leaderboard_type"]}
+**ID:** {character["character_info"]["id"]}
+\n"""
+    return message
+
+
+def leaderboard_lookup(username: str, info_details: str = None) -> str:
+    """
+    Looks up the leaderboard information for a given username.
+
+    Args:
+        username (str): The username to lookup.
+        info_details (str, optional): Additional information details. Defaults to None. (Not implemented)
+
+    Returns:
+        str: The formatted character information.
+    """
+    user_characters = get_user_characters(username)
+    
+    highest_hc = None
+    highest_hc_alive = None
+    highest_sc = None
+    for character in user_characters:
+        if character["leaderboard_type"] == "Softcore" and (highest_sc is None or int(character["character_info"]["raptureLevel"]) > int(highest_sc["character_info"]["raptureLevel"])):
+            highest_sc = character
+        elif character["leaderboard_type"] == "Hardcore":
+            if highest_hc is None or int(character["character_info"]["raptureLevel"]) > int(highest_hc["character_info"]["raptureLevel"]):
+                highest_hc = character
+            if character["character_info"]["deaths"] == '0' and (highest_hc_alive is None or int(character["character_info"]["rating"]) > int(highest_hc_alive["character_info"]["rating"])):
+                highest_hc_alive = character
+
+    highest_characters = []
+    if highest_sc:
+        highest_characters.append(highest_sc)
+    if highest_hc and highest_hc_alive and highest_hc['character_info']['id'] == highest_hc_alive['character_info']['id']:
+        highest_characters.append(highest_hc_alive)
+        return format_character_info_base(highest_characters)
+    if highest_hc:
+        highest_characters.append(highest_hc)
+    if highest_hc_alive:
+        highest_characters.append(highest_hc_alive)
+    if not info_details:
+        message = format_character_info_base(highest_characters)
+        #print(message)
+        return message
         
 def send_discord_followup(request_body, content):
+    """
+    Sends a follow-up message to a Discord channel.
+
+    Args:
+        request_body (dict): The request body containing the application ID and token.
+        content (str): The content of the follow-up message.
+
+    Returns:
+        str: The response message indicating the success or failure of sending the follow-up message.
+    """
     try:
         logging.info("Starting to send Discord follow-up")
 
@@ -201,7 +320,17 @@ def interact(raw_request):
                 message_content = "[Github Repo](https://github.com/daniellindem/dwarvenrealms-discord-bot)"
 
             case "leaderboard":
-                message_content = "[Leaderboard](https://dwarvenleaderboard.com/)"
+                try:
+                    data_options = data.get("options", [{}])
+                    logging.debug(f"Data options: {data_options}")
+                    username = data_options[0].get("value", "")
+                    if len(data_options) > 1:
+                        info_details = data_options[1].get("value", None)
+                        message_content = leaderboard_lookup(username, info_details)
+                    else:
+                        message_content = leaderboard_lookup(username)
+                except:
+                    message_content = "[Leaderboard](https://dwarvenleaderboard.com/)"
 
             case "help":
                 try:
--- test_leaderboard.py
+++ test_leaderboard.py
@@ -1,6 +1,7 @@
 import requests
+import pprint
 
-def get_user_info(username: str):
+def get_user_characters(username: str):
     # URLs for normal and hardcore leaderboards
     softcore_url = "http://loadbalancer-e2a9b2a-1115437761.us-east-1.elb.amazonaws.com/leaderboards/scores?type=normal"
     hardcore_url = "http://loadbalancer-e2a9b2a-1115437761.us-east-1.elb.amazonaws.com/leaderboards/scores?type=hardcore"
@@ -25,73 +26,76 @@ def get_user_info(username: str):
         for leaderboard in softcore_data["leaderboards"]:
             if str(leaderboard["name"].split()[0]).lower() == username_lower:
                 user_info_list.append({"leaderboard_type": "Softcore", "character_info": leaderboard, "ranking": sc_ranking})
-                return user_info_list
             sc_ranking += 1
 
         # Search for username in hardcore leaderboard
         for leaderboard in hardcore_data["leaderboards"]:
             if str(leaderboard["name"].split()[0]).lower() == username_lower:
                 user_info_list.append({"leaderboard_type": "Hardcore", "character_info": leaderboard, "ranking": hc_ranking})
-                if leaderboard['deaths'] == '0':
-                    user_info_list = [{"leaderboard_type": "Hardcore", "character_info": leaderboard, "ranking": hc_ranking}]
-                    return user_info_list
             hc_ranking += 1
 
         if user_info_list:
-            return user_info_list[0]
+            return user_info_list
         else:
             return f"No characters found for user '{username}' in leaderboards."
     else:
         return "Failed to retrieve leaderboard data."
     
-def format_character_info(info):
-    character_info = info['character_info']
-    # Format tokens list
-    tokens_list = '\n'.join([f"- {token}" for token in character_info['tokens']])
+
     
-    # Format build dictionary
-    build_info = '\n'.join([f"- **{key} Mod:** {value}" for key, value in character_info['build'].items()])
+def format_character_info_base(highest_characters):
+    highest_characters_sorted = sorted(highest_characters, key=lambda x: int(x["character_info"]["raptureLevel"]), reverse=True)
+    #print(highest_characters_sorted)
+    message = ""
+    for character in highest_characters_sorted:
+        if character["leaderboard_type"] == "Hardcore":
+            if character["character_info"]["deaths"] == "0":
+                alive_status = "Alive"
+            else:
+                alive_status = "Dead"
+            message += f"""**Name:** {character["character_info"]["name"]}
+**Rapture Level:** {character["character_info"]["raptureLevel"]}
+**Ranking:** {character["ranking"]}
+**Leaderboard:** {character["leaderboard_type"]}
+**Status:** {alive_status}
+**ID:** {character["character_info"]["id"]}
+\n"""
+        else:
+            message += f"""**Name:** {character["character_info"]["name"]}
+**Rapture Level:** {character["character_info"]["raptureLevel"]}
+**Ranking:** {character["ranking"]}
+**Leaderboard:** {character["leaderboard_type"]}
+**ID:** {character["character_info"]["id"]}
+\n"""
+    return message
+
+
+def leaderboard_lookup(username: str, info_details: str = None) -> str:
+    user_characters = get_user_characters(username)
     
-    # Construct the Discord markdown message
-    discord_message = f"""**Name:** {character_info['name']}
-**Rapture Level:** {character_info['raptureLevel']}
-**Ranking:** {info['ranking']}
-**Leaderboard:** {info['leaderboard_type']}
+    highest_hc = None
+    highest_hc_alive = None
+    highest_sc = None
+    for character in user_characters:
+        if character["leaderboard_type"] == "Softcore" and (highest_sc is None or int(character["character_info"]["raptureLevel"]) > int(highest_sc["character_info"]["raptureLevel"])):
+            highest_sc = character
+        elif character["leaderboard_type"] == "Hardcore":
+            if highest_hc is None or int(character["character_info"]["raptureLevel"]) > int(highest_hc["character_info"]["raptureLevel"]):
+                highest_hc = character
+            if character["character_info"]["deaths"] == '0' and (highest_hc_alive is None or int(character["character_info"]["rating"]) > int(highest_hc_alive["character_info"]["rating"])):
+                highest_hc_alive = character
 
-**More Information:**
-**Character Info:**
-- **Level:** {character_info['level']}
-- **Deaths:** {character_info['deaths']}
-- **Zone:** {character_info['zone']}
-- **Stance:** {character_info['stance']}
-- **Is Hardcore:** {'Yes' if character_info['isHardcore'] else 'No'}
-
-**Tokens:**
-{tokens_list}
-
-**Build:**
-{build_info}
-"""
-
-    return discord_message
-
-
-# Example usage
-username = "Magy"  # Input username here
-user_info = get_user_info(username)
-for info in user_info:
-    print(info)
-    #print(f"Leaderboard Type: {info['leaderboard_type']}")
-    #print(info["character_info"])
-    leaderboard = info['leaderboard_type']
-    character_info = info['character_info']
-    ranking = info['ranking']
-    #print()
-    if leaderboard == 'Hardcore' and character_info['deaths'] == '1':
-        deadchar = ' (DEAD)'
-    else:
-        deadchar = ''
-    print(f"Character: {character_info['name']}{deadchar}\nRank: {ranking}\nLeaderboard: {leaderboard}\nMax rapture: {character_info['raptureLevel']}\nLevel: {character_info['level']}, Deaths: {character_info['deaths']}\n\n")
-    
-    #print("\n\n\n\n\n\n")
-    #print(format_character_info(info))
+    highest_characters = []
+    if highest_sc:
+        highest_characters.append(highest_sc)
+    if highest_hc and highest_hc_alive and highest_hc['character_info']['id'] == highest_hc_alive['character_info']['id']:
+        highest_characters.append(highest_hc_alive)
+        return format_character_info_base(highest_characters)
+    if highest_hc:
+        highest_characters.append(highest_hc)
+    if highest_hc_alive:
+        highest_characters.append(highest_hc_alive)
+    if not info_details:
+        message = format_character_info_base(highest_characters)
+        #print(message)
+        return message
